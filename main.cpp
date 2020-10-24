822e1e63d6f64fd5de0386c78f51a754784c0293
Angel Armando <angel.gomez1394@alumnos.udg.mx>
2020-10-24 17:54:08 -0500

    se añadieron mas objetos

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 int main(){
     arreglo_dinamico arreglo;
+    arreglo.insertar_final("e");
+    arreglo.insertar_final("n");
     arreglo.insertar_final("o");
-    arreglo.insertar_final("l");
+    arreglo.insertar_final("s");
+    arreglo.insertar_final("d");
+    arreglo.insertar_final("i");
     arreglo.insertar_final("a");
-    arreglo.insertar_inicio("h");
+    arreglo.insertar_final("s");
+    arreglo.insertar_inicio("u");
+    arreglo.insertar_inicio("b");
 
     for (size_t i = 0; i < arreglo.size(); i++)
     {
